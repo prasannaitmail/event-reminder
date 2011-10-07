942ac33da0acc41f71ae59a3bcafd410cdda21d0
raneprajakta06 <raneprajakta06@8e533b97-2877-3d6b-6d6a-26777f90f747>
2011-10-07 06:54:36 +0000

    Added options menu methods

--- trunk/EventReminder/src/com/example/android/eventreminder/MainActivity.java
+++ trunk/EventReminder/src/com/example/android/eventreminder/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View;
@@ -23,7 +26,7 @@ public class MainActivity extends ListActivity {
         ArrayAdapter<String> adapter =
         new ArrayAdapter<String>(this, R.layout.list_row, R.id.eventrow, listItems); 
         setListAdapter(adapter);
-    }
+    }//end of OnCreate
         
     /*Method for short clicks*/
     @Override
@@ -31,8 +34,27 @@ public class MainActivity extends ListActivity {
     super.onListItemClick(l, v, position, id);
     /*Start Edit Activity*/
     Intent intent = new Intent(this, AddReminder.class);
-    //intent.putExtra(“RowId”, id);
     startActivity(intent);
+    }//end of onListItemClick
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    MenuInflater mi = getMenuInflater();
+    mi.inflate(R.menu.menu, menu);
+    return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) { 
+    switch(item.getItemId()) {
+    case R.id.addEvent:
+    	Intent intent = new Intent(this, AddReminder.class);
+        startActivity(intent);
+  
+    return true;
+    }
+    return super.onMenuItemSelected(featureId, item);
     }
     
 }
