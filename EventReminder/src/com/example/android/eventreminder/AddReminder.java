7b644aa9d50c1722cd5b893fb9fd661b1d9340f8
raneprajakta06 <raneprajakta06@8e533b97-2877-3d6b-6d6a-26777f90f747>
2011-10-07 03:28:31 +0000

    Initial import

---
+++ EventReminder/src/com/example/android/eventreminder/AddReminder.java
@@ -0,0 +1,13 @@
+package com.example.android.eventreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class AddReminder extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.add_reminder);
+	}
+}
