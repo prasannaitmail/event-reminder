0c25766eca67a0481012ab2f3306898159a71a0f
raneprajakta06 <raneprajakta06@8e533b97-2877-3d6b-6d6a-26777f90f747>
2011-10-21 16:05:40 +0000

    Updated date picker and time picker functionality, Added "add event" and "update event" related functionality

--- EventReminder/src/com/example/android/eventreminder/AddReminder.java
+++ EventReminder/src/com/example/android/eventreminder/AddReminder.java
@@ -2,13 +2,18 @@ package com.example.android.eventreminder;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.ParseException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -18,134 +23,225 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class AddReminder extends Activity {
+	String RowID;
+	private Long mRowId;
+	
+	private Calendar mCalendar;
+		
 	private Button mPickDateButton;
 	private Button mPickTimeButton;
 		
-    private int mYear;
-    private int mMonth;
-    private int mDay;
-    private int mHour;
-    private int mMinute;
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIME_FORMAT = "kk:mm";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd kk:mm:ss";
 
     static final int DATE_DIALOG_ID = 0;
     static final int TIME_DIALOG_ID = 1;
     
     private EditText mTitleText;
-    private Button mSaveButton;
     private EditText mNoteText;
+    private Button mSaveButton;
+    private Button mCancelButton;
     private eventDB mDbHelper;
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
+	
+	mDbHelper = new eventDB(this);
+	
 	setContentView(R.layout.add_reminder);
 
-	mDbHelper = new eventDB(this);
+	mCalendar = Calendar.getInstance();
 	// capture our View elements
 	mPickDateButton = (Button) findViewById(R.id.dateForReminder);
 	mPickTimeButton = (Button) findViewById(R.id.timeForReminder);
-
-	mSaveButton = (Button) findViewById(R.id.save);
 	mTitleText = (EditText) findViewById(R.id.title);
 	mNoteText =(EditText) findViewById(R.id.notes);
+	mSaveButton = (Button) findViewById(R.id.save);
+	mCancelButton = (Button) findViewById(R.id.cancel);
 	
-	// add a click listener to the date button
-    mPickDateButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            showDialog(DATE_DIALOG_ID);
-        }
-    });
-
-    // add a click listener to the time button
-    mPickTimeButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            showDialog(TIME_DIALOG_ID);
-        }
-    });
-    // get the current date
-    final Calendar c = Calendar.getInstance();
-    mYear = c.get(Calendar.YEAR);
-    mMonth = c.get(Calendar.MONTH);
-    mDay = c.get(Calendar.DAY_OF_MONTH);
-    // get the current time
-    mHour = c.get(Calendar.HOUR_OF_DAY);
-    mMinute = c.get(Calendar.MINUTE);
-
-    
- // display the current date (this method is below)
-    updateDateButtonDisplay();
+	mRowId = savedInstanceState != null 
+	? savedInstanceState.getLong(eventDB.KEY_ROWID)
+	: null;
 	
-    // display the current date
-    updateTimeButtonDisplay();
-
-         
+		if(getIntent()!= null){
+			Bundle extras = getIntent().getExtras();
+			int rowID = extras != null ? extras.getInt(RowID) : -1;
+		}
+	
+		registerButtonListenersAndSetDefaultText();
 	}//End of onCreate()
-
-	// updates the date in the TextView
-    private void updateDateButtonDisplay() {
-        mPickDateButton.setText(
-            new StringBuilder()
-                    // Month is 0 based so add 1
-                    .append(mMonth + 1).append("-")
-                    .append(mDay).append("-")
-                    .append(mYear).append(" "));
-    }
 	
- // updates the time we display in the TextView
-    private void updateTimeButtonDisplay() {
-        mPickTimeButton.setText(
-            new StringBuilder()
-                    .append(pad(mHour)).append(":")
-                    .append(pad(mMinute)));
-    }
+	private void setRowIdFromIntent() { 
+		if (mRowId == null) {
+		Bundle extras = getIntent().getExtras();
+		mRowId = extras != null	? extras.getLong(eventDB.KEY_ROWID)	: null;
+		}
+		
+		if(mRowId == null)
+			setTitle("Add New Event");
+		else
+			setTitle("Edit Event");
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mDbHelper.close(); 
+	}
+	@Override
+	protected void onResume() { 
+		super.onResume();
+		mDbHelper.open(); 
+		setRowIdFromIntent(); 
+		showFields(); 
+	}
+	
+    private void registerButtonListenersAndSetDefaultText() {
+		// TODO Auto-generated method stub
+    	/* click listener to the Date button*/
+    	mPickDateButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+		/* Click listener to the Time button */
+		mPickTimeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			showDialog(TIME_DIALOG_ID);
+			}
+		});
+		
+		/* Click listener to the Save button */
+		mSaveButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				saveEvent(); 
+				setResult(RESULT_OK); 
+				Toast.makeText(AddReminder.this, 
+						getString(R.string.event_saved_message),
+						Toast.LENGTH_SHORT).show();
+				finish(); 
+			}
+		});
+		
+		/* Click listener to the Save button */
+		mCancelButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Toast.makeText(AddReminder.this, 
+						getString(R.string.event_canceled_message),
+						Toast.LENGTH_SHORT).show();
+				finish(); 
+			}
+		});
+		updateDateButtonText();
+		updateTimeButtonText();
+	}
 
-    private static String pad(int c) {
-        if (c >= 10)
-            return String.valueOf(c);
-        else
-            return "0" + String.valueOf(c);
+    @Override
+    protected Dialog onCreateDialog(int id) { 
+    	switch(id) {
+    		case DATE_DIALOG_ID: 
+    			return showDatePicker();
+    		case TIME_DIALOG_ID:
+    			return showTimePicker();
+    	}
+    	return super.onCreateDialog(id);
     }
     
-	// the callback received when the user "sets" the date in the dialog
-    private DatePickerDialog.OnDateSetListener mDateSetListener =
-            new DatePickerDialog.OnDateSetListener() {
-
-                public void onDateSet(DatePicker view, int year, 
-                                      int monthOfYear, int dayOfMonth) {
-                    mYear = year;
-                    mMonth = monthOfYear;
-                    mDay = dayOfMonth;
-                    updateDateButtonDisplay();
-                }
-            };
-            
-  // the callback received when the user "sets" the time in the dialog
-    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
-    	new TimePickerDialog.OnTimeSetListener() {
-        	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        			mHour = hourOfDay;
-                    mMinute = minute;
-                    updateTimeButtonDisplay();
-                    }
-                };
-
-            
-    @Override
-    protected Dialog onCreateDialog(int id) {
-    	switch (id) {
-        	case DATE_DIALOG_ID:
-        		return new DatePickerDialog(this,
-        				mDateSetListener,
-                        mYear, mMonth, mDay);
-        	case TIME_DIALOG_ID:
-                return new TimePickerDialog(this,
-                        mTimeSetListener, mHour, mMinute, false);
-        		}
-    	return null;
+    private DatePickerDialog showDatePicker() { 
+    	DatePickerDialog datePicker = new DatePickerDialog(AddReminder.this,
+    	new DatePickerDialog.OnDateSetListener() {
+    		@Override
+    		public void onDateSet(DatePicker view, int year, int monthOfYear,
+    		int dayOfMonth) { 
+    		mCalendar.set(Calendar.YEAR, year); 
+    		mCalendar.set(Calendar.MONTH, monthOfYear);
+    		mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+    		updateDateButtonText(); 
+    		}
+    	},mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+    	mCalendar.get(Calendar.DAY_OF_MONTH)); 
+    	return datePicker;
+    }
+    
+    private TimePickerDialog showTimePicker() {
+    	TimePickerDialog timePicker = new TimePickerDialog(this, new
+    	TimePickerDialog.OnTimeSetListener() { 
+    	@Override
+    	public void onTimeSet(TimePicker view, int hourOfDay, int minute){ 
+    		mCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay); 
+    		mCalendar.set(Calendar.MINUTE, minute); 
+    		updateTimeButtonText(); 
     	}
-           
-       	
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+    	}, mCalendar.get(Calendar.HOUR_OF_DAY), 
+    		mCalendar.get(Calendar.MINUTE), true); 
+    	return timePicker;
+    	}
+    
+    private void updateDateButtonText() { 
+    	SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT); 
+    	String dateForButton = dateFormat.format(mCalendar.getTime()); 
+    	mPickDateButton.setText(dateForButton);
+    	}
+    
+    private void updateTimeButtonText() {
+    	SimpleDateFormat timeFormat = new SimpleDateFormat(TIME_FORMAT); 
+    	String timeForButton = timeFormat.format(mCalendar.getTime()); 
+    	mPickTimeButton.setText(timeForButton); 
+    	}
+    
+    private void showFields(){ 
+    	if (mRowId != null) {
+    		Cursor reminder = mDbHelper.fetchEvent(mRowId); 
+    		startManagingCursor(reminder);
+    		mTitleText.setText(reminder.getString(
+    				reminder.getColumnIndexOrThrow(eventDB.KEY_TITLE))); 
+    		mNoteText.setText(reminder.getString(
+    				reminder.getColumnIndexOrThrow(eventDB.KEY_NOTE))); 
+    		SimpleDateFormat dateTimeFormat = new SimpleDateFormat(DATE_TIME_FORMAT);
+    		Date date = null; 
+    		try {
+    			String dateString = reminder.getString(
+    					reminder.getColumnIndexOrThrow(
+    							eventDB.KEY_DATE_TIME)); 
+    			date = dateTimeFormat.parse(dateString);
+    			mCalendar.setTime(date);
+    		} catch (java.text.ParseException e) { 
+    			Log.e("AddReminder", e.getMessage(), e); 
+    		}
+    	}
+    	updateDateButtonText();
+    	updateTimeButtonText();
+    }
+   
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+    	super.onSaveInstanceState(outState);
+    	outState.putLong(eventDB.KEY_ROWID, mRowId); 
+    }
+    
+    /* Saves the event */
+    private void saveEvent() {
+    	String title = mTitleText.getText().toString(); 
+    	String body = mNoteText.getText().toString(); 
+    	SimpleDateFormat dateTimeFormat = new SimpleDateFormat(DATE_TIME_FORMAT); 
+    	String eventDateTime = dateTimeFormat.format(mCalendar.getTime()); 
+    	if (mRowId == null) {
+    		long id = mDbHelper.createEvent(title, body, eventDateTime); 
+    		if (id > 0) { 
+    			mRowId = id; 
+    		}
+    	}
+    	else {
+    		mDbHelper.updateEvent(mRowId, title, body, eventDateTime); 
+    	}
+    }//end of saveEvent
+      
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent){
     	super.onActivityResult(requestCode, resultCode, intent);
 	
 	}
